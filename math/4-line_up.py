3f5a58047b5c7019af0dfcf209c9503af0386060
malekmrabti213 <malek6mrabti@gmail.com>
2025-07-21 01:09:31 +0100

    c

--- math/4-line_up.py
+++ math/4-line_up.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+""" function to add two arrays"""
+
+
 def add_arrays(arr1, arr2):
-    #addition in 1D array
-    sumadd=[]
-    if len(arr1)!=len(arr2):
+    """ function : add two array
+
+    Arguments:
+        arr1 : first array
+        arr2 : second array
+
+    Returns:
+        new list
+    """
+    if len(arr1) != len(arr2):
         return None
-    else:
-        for i in range(len(arr1)):
-            sumadd.append(arr1[i]+arr2[i])
-        return sumadd
+
+    result = []
+    for i in range(len(arr1)):
+        result.append(arr1[i] + arr2[i])
+    return result
